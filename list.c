094cecd9d221d3c547a9007676d6d5f8dab89ed4
Filipe Maia <filipe@xray.bmc.uu.se>
2009-03-18 20:51:29 +0000

    Added list.c
    git-svn-id: svn://hirst/libspimage/trunk@471 3f9d24b3-d911-0410-9091-ffa98efebf22

---
+++ list.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#ifdef _USE_DMALLOC
+#include <dmalloc.h>
+#endif
+#include "spimage.h"
+
+
+
+void sp_list_sort(sp_list * l){
+  /* use bubble sort! */
+  int swapped;
+  do{
+    swapped = 0;
+    for(unsigned int i = 0;i<sp_list_size(l)-1;i++){
+      if(sp_list_get(l,i) > sp_list_get(l,i+1)){
+	real tmp = sp_list_get(l,i);
+	sp_list_set(l,i,sp_list_get(l,i+1));
+	sp_list_set(l,i+1,tmp);
+	swapped = 1;
+      }
+    }
+  }while(swapped);
+}
+
+void sp_list_union(sp_list * l, sp_list * m){
+  for(unsigned int i = 0;i<sp_list_size(m);i++){
+    sp_list_remove_all(l,sp_list_get(m,i));
+    sp_list_append(l,sp_list_get(m,i));
+  }
+  sp_list_sort(l);
+}
