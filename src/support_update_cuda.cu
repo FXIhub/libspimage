63a724387a416ee9de66429f70ac67e2324ffee8
Filipe Maia <filipe@xray.bmc.uu.se>
2009-09-27 18:36:34 +0200

    Improvements to build in Leopard

--- src/support_update_cuda.cu
+++ src/support_update_cuda.cu
@@ -50,16 +50,16 @@ static real bezier_map_interpolation(sp_smap * map, real x){
 		2*pow(p0x,3) - 2*p0x*pow(p3x,2) - 4*x*pow(p3x,2) + 
 		2*pow(p3x,3) + pow(pow(p0x - p3x,4)*
 				   (6*p0x*p3x - 16*p0x*x - 16*p3x*x + 5*pow(p0x,2) +
-				    5*pow(p3x,2) + 16*pow(x,2)),0.5),
-		-0.3333333333333333) +
+				    5*pow(p3x,2) + 16*pow(x,2)),0.5f),
+		-0.3333333333333333f) +
 	    2*pow(p0x - p3x,-1)*
 	    pow(8*p0x*p3x*x - 2*p3x*pow(p0x,2) - 4*x*pow(p0x,2) +
 		2*pow(p0x,3) - 2*p0x*pow(p3x,2) - 4*x*pow(p3x,2) +
 		2*pow(p3x,3) + pow(pow(p0x - p3x,4)*
 				   (6*p0x*p3x - 16*p0x*x -
 				    16*p3x*x + 5*pow(p0x,2) + 
-				     5*pow(p3x,2) + 16*pow(x,2)),0.5)
-		,0.3333333333333333)
+				     5*pow(p3x,2) + 16*pow(x,2)),0.5f)
+		,0.3333333333333333f)
 	    )/4.;  
   return 3*p0y*t*pow(1 - t,2) + p0y*pow(1 - t,3) +
     3*p3y*(1 - t)*pow(t,2) + p3y*pow(t,3);
