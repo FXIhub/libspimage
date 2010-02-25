dfea38327b8e01722a0754fee11078486886be71
Tomas Ekeberg <ekeberg@tove.(none)>
2010-02-25 23:09:20 +0100

    ...

--- include/spimage/phasing.h
+++ include/spimage/phasing.h
@@ -138,6 +138,7 @@ spimage_EXPORT int sp_phaser_iterate(SpPhaser * ph, int iterations);
   int sp_support_threshold_update_support(SpSupportAlgorithm *alg, SpPhaser * ph);
   int sp_support_template_update_support(SpSupportAlgorithm *alg, SpPhaser * ph);
   int sp_support_static_update_support(SpSupportAlgorithm *alg, SpPhaser * ph);
+  int sp_support_close_update_support(SpSupportAlgorithm *alg, SpPhaser * ph);
 
   int sp_support_array_update(SpSupportArray *array, SpPhaser *ph);
 
