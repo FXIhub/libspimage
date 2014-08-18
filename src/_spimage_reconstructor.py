9157eb621283c4d79bea0e0d606196f2ddf990c0
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2014-08-19 00:13:53 +0200

    typo

--- src/_spimage_reconstructor.py
+++ src/_spimage_reconstructor.py
@@ -380,7 +380,7 @@ class Reconstructor:
             self._sp_initial_support.image[:] = np.float32(np.fft.fftshift(R) < self._initial_support_config["radius"])
         else:
             self._phaser_dirty = True
-v            S = self._initial_support_config["support_mask"]
+            S = self._initial_support_config["support_mask"]
             if self._initial_support_config["support_mask_shifted"]:
                 S = np.fft.fftshift(S)
             self.sep_initial_support.image[:] = np.float32(S)
