c63050209952592734a490a5395ecb60c0daa864
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2014-08-21 20:12:47 +0200

    typo

--- src/_spimage_reconstructor.py
+++ src/_spimage_reconstructor.py
@@ -591,7 +591,7 @@ class Reconstructor:
         support_final = np.zeros(shape=(Nrepeats,self._Nx,self._Ny),dtype="bool")
         for self._reconstruction in range(Nrepeats):
             self._log("Reconstruction %i started" % (self._reconstruction),"INFO")
-            output = self.reconstruct())
+            output = self.reconstruct()
             real_space_final[self._reconstruction,:,:] = output["real_space"][-1,:,:]
             support_final[self._reconstruction,:,:] = output["support"][-1,:,:]
             outputs.append(output)
