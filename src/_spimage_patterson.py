e053118521817c3dc6a9582cb2915c01b95e736c
Max Hantke <maxhantke@gmail.com>
2015-03-22 12:06:52 +0900

    Added optional additional output from patterson() for tern.

--- src/_spimage_patterson.py
+++ src/_spimage_patterson.py
@@ -2,13 +2,19 @@ import numpy
 import scipy.signal
 import scipy.ndimage
 
-def patterson(I,M,image_threshold=0.,mask_smooth=1.,mask_threshold=1.,darkfield_x=None,darkfield_y=None,darkfield_sigma=None,normalize_median=False):
+def patterson(I,M,image_threshold=0.,mask_smooth=1.,mask_threshold=1.,darkfield_x=None,darkfield_y=None,darkfield_sigma=None,normalize_median=False, full_output=False):
     I = numpy.clip(I, image_threshold, numpy.inf)
     K = kernel(M,smooth=mask_smooth,threshold=mask_threshold,x=darkfield_x,y=darkfield_y,sigma=darkfield_sigma)
     P = numpy.fft.fftshift(numpy.fft.fft2(numpy.fft.fftshift(K*I)))
     if normalize_median:
         P /= numpy.median(abs(P))
-    return P
+    if full_output:
+        info = {}
+        info["kernel"] = I
+        info["intensities"] = K*I
+        return P, info
+    else:
+        return P
 
 def kernel(mask,smooth=1.,threshold=1.,x=None,y=None,sigma=None):
     K = scipy.ndimage.gaussian_filter(numpy.array(mask,dtype="float"),smooth)
