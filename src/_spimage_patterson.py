be7d8468b4102e490c5defeedca742119a523895
Max Hantke <maxhantke@gmail.com>
2015-03-23 19:07:05 +0900

    Improved patterson, new feature: fourier kernel subtraction.

--- src/_spimage_patterson.py
+++ src/_spimage_patterson.py
@@ -2,12 +2,16 @@ import numpy
 from scipy.ndimage.filters import gaussian_filter
 from scipy.ndimage.morphology import distance_transform_edt
 
-def patterson(image, mask, floor_cut=0., mask_smooth=1., darkfield_x=None, darkfield_y=None, darkfield_sigma=None, normalize_median=False, radial_boost=False, full_output=False):
+def patterson(image, mask, floor_cut=0., mask_smooth=1., darkfield_x=None, darkfield_y=None, darkfield_sigma=None, normalize_median=False, radial_boost=False, subtract_fourier_kernel=False, full_output=False):
+    info = {}
     # Making sure that we leave memory of the input arrays untouched
     M = numpy.array(mask.copy(),dtype=numpy.float64)
     I = numpy.array(image.copy(),dtype=numpy.float64)
     # Clipping
-    I = numpy.clip(I*M, floor_cut, numpy.inf)
+    #I = numpy.clip(I*M, floor_cut, numpy.inf)
+    tmp = I <= floor_cut
+    if tmp.sum() > 0:
+        I[tmp] = 0.
     if radial_boost:
         # Radial boost: Multiply intensities by radius**4
         Ny, Nx = I.shape
@@ -22,12 +26,20 @@ def patterson(image, mask, floor_cut=0., mask_smooth=1., darkfield_x=None, darkf
     K = kernel(M,smooth=mask_smooth,x=darkfield_x,y=darkfield_y,sigma=darkfield_sigma)
     # Fourier transform
     P = numpy.fft.fftshift(numpy.fft.fftn(K*I))
+    if subtract_fourier_kernel:
+        fK = numpy.fft.fftshift(numpy.fft.fftn(K))
+        if full_output:
+            info["patterson0"] = P.copy()
+            if normalize_median:
+                info["patterson0"] /= numpy.median(abs(P))
+        s_fK = abs(fK[fK.shape[0]/2,:]).sum() + abs(fK[:,fK.shape[1]/2]).sum()
+        s_P = abs(P[P.shape[0]/2,:]).sum() + abs(P[:,P.shape[1]/2]).sum()
+        P = abs(P) - abs(fK / s_fK * s_P)
     if normalize_median:
         # Normalization
         P /= numpy.median(abs(P))
     if full_output:
-        info = {}
-        info["kernel"] = I
+        info["kernel"] = K
         info["intensities"] = K*I
         P_nk = numpy.fft.fftshift(numpy.fft.fft2(numpy.fft.fftshift(numpy.array(image,dtype=numpy.float64))))
         if normalize_median:
