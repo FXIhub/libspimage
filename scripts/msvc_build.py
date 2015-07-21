325eec80f3c0f422a6b45fa58d5d2f28c04253fe
Filipe Maia <filipe.c.maia@gmail.com>
2015-07-21 22:52:33 +0200

    Change build scipt to 64bits

--- scripts/msvc_build.py
+++ scripts/msvc_build.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 
+import os
+import subprocess
+
 env_map = os.environ;
 
 p = subprocess.Popen(['cmake',
                   '-G','Visual Studio 12 Win64',
-                  '-DTIFF_LIBRARY:FILEPATH=c:/Program Files (x86)/GnuWin32/lib/libtiff.lib',
-                  '-DTIFF_INCLUDE_DIR:PATH=c:/Program Files (x86)/GnuWin32/include',
-                  '-DJPEG_LIBRARY:FILEPATH=c:/Program Files (x86)/GnuWin32/bin/jpeg.lib',
-                  '-DJPEG_INCLUDE_DIR:PATH=c:/Program Files (x86)/GnuWin32/include',
-                  '-DPNG_LIBRARY:FILEPATH=c:/Program Files (x86)/GnuWin32/bin/libpng.lib',
-                  '-DPNG_PNG_INCLUDE_DIR:PATH=c:/Program Files (x86)/GnuWin32/include',
-                  '-DZLIB_LIBRARY:FILEPATH=c:/Program Files (x86)/GnuWin32/bin/zlib.lib',
-                  '-DZLIB_INCLUDE_DIR:PATH=c:/Program Files (x86)/GnuWin32/include',
-                  '-DFFTW3_FFTWF_LIBRARY:FILEPATH=c:/fftw3/libfftw3f-3.lib',
-                  '-DFFTW3_FFTW_LIBRARY:FILEPATH=c:/fftw3/libfftw3-3.lib',
-                  '-DFFTW3_FFTWL_LIBRARY:FILEPATH=c:/fftw3/libfftw3l-3.lib',
-                  '-DFFTW3_INCLUDE_DIR:PATH=c:/fftw3',
-                  '-DHDF5_LIBRARY:FILEPATH=c:/Program Files (x86)/HDF_Group/HDF5/1.8.15/lib/hdf5.lib',
-                  '-DHDF5_INCLUDE_DIR:PATH=c:/Program Files (x86)/HDF_Group/HDF5/1.8.15/include',
-                  '-DGSL_GSL_LIBRARY:FILEPATH=c:/Program Files (x86)/GnuWin32/lib/libgsl.lib',
-                  '-DGSL_GSLCBLAS_LIBRARY:FILEPATH=c:/Program Files (x86)/GnuWin32/lib/libgslcblas.lib',
-                  '-DGSL_INCLUDE_DIR:PATH=c:/Program Files (x86)/GnuWin32/include',
+                  '-DTIFF_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libtiff.dll.a',
+                  '-DTIFF_INCLUDE_DIR:PATH=c:/msys64/mingw64/include',
+                  '-DPNG_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libpng.dll.a',
+                  '-DPNG_PNG_INCLUDE_DIR:PATH=c:/msys64/mingw64/include',
+                  '-DZLIB_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libz.dll.a',
+                  '-DZLIB_INCLUDE_DIR:PATH=c:/msys64/mingw64/include',
+                  '-DFFTW3_FFTWF_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libfftw3f.dll.a',
+                  '-DFFTW3_FFTW_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libfftw3.dll.a',
+                  '-DFFTW3_FFTWL_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libfftw3l.dll.a',
+                  '-DFFTW3_INCLUDE_DIR:PATH=c:/msys64/mingw64/include',
+                  '-DHDF5_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libhdf5.dll.a',
+                  '-DHDF5_INCLUDE_DIR:PATH=c:/msys64/mingw64/include',
+                  '-DGSL_GSL_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libgsl.dll.a',
+                  '-DGSL_GSLCBLAS_LIBRARY:FILEPATH=c:/msys64/mingw64/lib/libgslcblas.dll.a',
+                  '-DGSL_INCLUDE_DIR:PATH=c:/msys64/mingw64/include',
                   '-DCMAKE_VERBOSE_MAKEFILE:BOOL=FALSE',
                   '..'], env=env_map)
 
