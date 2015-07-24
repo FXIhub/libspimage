89a4c9a8ced70ecc8ca99e57a6244165979d5eed
Filipe Maia <filipe.c.maia@gmail.com>
2015-07-24 12:15:40 +0200

    Correct wrong path

--- scripts/msvc_build_32.py
+++ scripts/msvc_build_32.py
@@ -123,7 +123,7 @@ if not os.path.exists('c:/fftw3-32'):
 # Install CUDA 6
 cuda_url = 'http://developer.download.nvidia.com/compute/cuda/6_0/rel/installers/cuda_6.0.37_winvista_win7_win8.1_general_64.exe'
 cuda_exe = 'cuda_6.0.37_winvista_win7_win8.1_general_64.exe'
-if not os.path.exists('c:/Files)/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v6.0'):
+if not os.path.exists('c:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v6.0'):
     if not os.path.exists(cuda_exe):
         geturl(cuda_url,cuda_exe);
     os.system(cuda_exe);
