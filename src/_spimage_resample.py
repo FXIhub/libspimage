93e88ab765abbc77ec4532bdc39c39716037d8d7
Louis Doctor <doctor@i001.davinci>
2016-12-01 13:37:13 +0100

    extended the 3d case

--- src/_spimage_resample.py
+++ src/_spimage_resample.py
@@ -6,7 +6,7 @@ def downsample(array,factor,mode="pick",**kwargs):
     if d == 2:
         return _downsample2d(array,factor,mode,**kwargs)
     elif d == 3:
-        return _downsample2d(array,factor,mode,**kwargs)
+        return _downsample3d(array,factor,mode,**kwargs)
     else:
         print "ERROR: Downsampling in %i dimensions is not supported."
         
@@ -85,32 +85,85 @@ def _downsample2d(array,factor,mode="pick",**kwargs):
             return [B.reshape((Ny_new,Nx_new)),BM.reshape((Ny_new,Nx_new))]
 
 
-def _downsample3d(array,factor,mask):
-    available_modes = ["pick"]
+def _downsample3d(array,factor,mode="pick",**kwargs):
+    available_modes = ["pick","integrate"]#,"interpolate"]                                                                                                                      
     if not mode in available_modes:
         print "ERROR: %s is not a valid mode." % mode
         return
+    mask         = kwargs.get("mask",None)
+    bad_bits     = kwargs.get("bad_bits",None)
+    min_N_pixels = kwargs.get("min_N_pixels",1)
     factor = int(round(factor))
     if factor == 1:
-        return array.copy()
-    Nz = array.shape[0]
-    Ny = array.shape[1]
+        if mask is None:
+            return array.copy()
+        else:
+            return [array.copy(),mask.copy()]
+    if mask is None:
+        mask = None
+    else:
+        mask = numpy.array(mask,dtype="int16")
     Nx = array.shape[2]
-    Nz_new = int(numpy.ceil(1.0*Nz/factor))
-    Ny_new = int(numpy.ceil(1.0*Ny/factor))
-    Nx_new = int(numpy.ceil(1.0*Nx/factor))  
-    array_new = numpy.zeros(shape=(Nz_new,Ny_new,Nx_new),dtype=array.dtype)
-    for z_new in numpy.arange(0,Nz_new,1):
-        for y_new in numpy.arange(0,Ny_new,1):
-            for x_new in numpy.arange(0,Nx_new,1):
-                z_min = z_new*factor
-                z_max = min([(z_new+1)*factor,Nz])
-                y_min = y_new*factor
-                y_max = min([(y_new+1)*factor,Ny])
-                x_min = x_new*factor
-                x_max = min([(x_new+1)*factor,Nx])
-                array_new[z_new,y_new,x_new] = array[z_min:z_max,y_min:y_max,x_min:x_max].sum()/(1.*mask[z_min:z_max,y_min:y_max,x_min:x_max].sum())
-    return array_new
+    Ny = array.shape[1]
+    Nz = array.shape[0]
+    if mode == "pick":
+        Z,Y,X = numpy.indices(array.shape)
+        pick = ((Z%factor == 0)*(Y%factor == 0)*(X%factor == 0))
+        Nz_new = pick[:,:,0].sum()
+        Ny_new = pick[:,0,:].sum()
+        Nx_new = pick[0,:,:].sum()
+        pick = pick.flatten()
+        A = array.flatten().copy()
+        array_new = (A[pick]).reshape((Nz_new,Ny_new,Nx_new))
+        if mask is not None:
+            M = mask.flatten().copy()
+            mask_new = (M[pick]).reshape((Nz_new,Ny_new,Nx_new))
+            return [array_new,mask_new]
+        else:
+            return array_new
+    elif mode == "integrate": # non-conservative if mask is given                                                                                                               
+        Nx_new = int(numpy.ceil(Nx/float(factor)))
+        Ny_new = int(numpy.ceil(Ny/float(factor)))
+        Nz_new = int(numpy.ceil(Nz/float(factor)))
+        Nx = Nx_new * factor
+        Ny = Ny_new * factor
+        Nz = Nz_new * factor
+        A = numpy.zeros(shape=(Nz,Ny,Nx),dtype=array.dtype)
+        A[:array.shape[0],:array.shape[1],:array.shape[2]] = array[:,:,:]
+        A = A.flat
+        Z,Y,X = numpy.indices((Nz,Ny,Nx))
+        Z = Z.flatten()
+        Y = Y.flatten()
+        X = X.flatten()
+        Z /= factor
+        Y /= factor
+        X /= factor
+        superp = Z*Ny+Y*Nx+X
+        superp_order = superp.argsort()
+        A = A[superp_order]
+        A = A.reshape((Nx_new*Ny_new*Nz_new,factor*factor*factor))
+        if mask is None:
+            B = A.sum(1)
+            return B.reshape((Nz_new,Ny_new,Nx_new))
+        if mask is not None:
+            AM = numpy.zeros(shape=(Nz,Ny,Nx),dtype="int16")
+            AM[:mask.shape[0],:mask.shape[1],:mask.shape[2]] = mask[:,:,:]
+            AM = AM.flat
+            AM = AM[superp_order]
+            AM = AM.reshape((Nx_new*Ny_new*Nz_new,factor*factor*factor))
+            if bad_bits is None:
+                B = (A*AM).sum(1)
+                BN = AM.sum(1)
+                BM = BN != 0
+            else:
+                B = (A*((AM & bad_bits) == 0)).sum(1)
+                BN = ((AM & bad_bits) == 0).sum(1)
+                BM = AM[:,:,0]
+                for i in range(1,factor*factor*factor):
+                    BM |= AM[:,:,i]
+                BM[BN >= min_N_pixels] = BM[BN >= min_N_pixels] & ~bad_bits
+            B[BN >= min_N_pixels] = B[BN >= min_N_pixels] * factor/numpy.float64(BN[BN >= min_N_pixels])
+            return [B.reshape((Nz_new,Ny_new,Nx_new)),BM.reshape((Nz_new,Ny_new,Nx_new))]
 
 def crop(pattern,cropLength,center='middle',bg=0):
     if cropLength > min(list(pattern.shape)):
